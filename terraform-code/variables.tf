d82a86e2c236c3fb6fcd38feec5ce76cad49a943
nilesh-bk9 <57229822+nilesh-bk9@users.noreply.github.com>
2021-03-22 17:51:43 +0530

    Create variables.tf
    added variables.tf

---
+++ terraform-code/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  default = "us-east-1"
+  description = "the region where you want deploy the solution"
+}
+
+variable "prefix" {
+    default = "codebuildTerraform"
+    description = "The prefix used to build the elements"
+}
+
+variable "profile" {
+  default = "ci-user"
+}
