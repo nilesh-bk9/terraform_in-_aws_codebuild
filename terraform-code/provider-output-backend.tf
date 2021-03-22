0faecf0d0e233a7ace8e38abc7c8deca113f1338
nilesh-bk9 <57229822+nilesh-bk9@users.noreply.github.com>
2021-03-22 18:12:00 +0530

    Update provider-output-backend.tf

--- terraform-code/provider-output-backend.tf
+++ terraform-code/provider-output-backend.tf
@@ -3,23 +3,3 @@ provider "aws" {
   profile    = "${var.profile}"
   region     = "${var.region}"
 }
-
-/* THIS SYNTAX IS NOT WORKING */
-/*
-terraform {
-  backend "s3" {
-    bucket = "${var.prefix}.${var.region}.terraform-state-nagra"
-    key    = "${var.prefix}/terraform.tfstate"
-    region = "${var.region}"
-  }
-}
-*/
-
-#this will be done by the script in the buldspec.yml
-#terraform {
-#  backend "s3" {
-#    bucket = "$datapart.$region.terraform-state-nagra"
-#    key    = "$applicationname/terraform.tfstate"
-#    region = "$region"
-#  }
-#}
